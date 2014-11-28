0e5aacb0298d30ad5e82e1a7e9c3f4db36a1c03a
kriloBT <krilocheng@bluetechnology.com.tw>
2014-11-28 15:06:07 +0800

    add bootstrap

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ ex4chart
 ========
 
 node.js + express 4 + C3.js
+
+C3.js:
+http://c3js.org/gettingstarted.html
+
+bootstrap use 'Yeti'
+http://bootswatch.com/yeti/
--- app.js
+++ app.js
@@ -8,11 +8,17 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+//add
+var partials = require('express-partials');
 
+var app = express();
+//add
+app.use(partials());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+//move
+app.use('/', routes);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,7 +28,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
